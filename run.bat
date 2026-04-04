679f5287f3bb7784e1af833775a97ed883ffb43d
bokwoon <bokwoon.c@gmail.com>
2026-04-05 00:31:20 +0800

    checkpoint

--- run.bat
+++ run.bat
@@ -1,2 +1,5 @@
 : #!/bin/sh
+:; CGO_ENABLED=0 wgo -xdir . npx @tailwindcss/cli --input ./static/notebrew.css --output ./static/notebrew.min.css --watch=always :: wgo -xdir . npx esbuild ./static/notebrew.js --bundle --outfile=./static/notebrew.min.js --watch=forever :: wgo -verbose -file .go go install -tags dev ./notebrew :: notebrew; exit
+@ECHO OFF
+set CGO_ENABLED=0
 wgo -xdir . npx @tailwindcss/cli --input ./static/notebrew.css --output ./static/notebrew.min.css --watch=always :: wgo -xdir . npx esbuild ./static/notebrew.js --bundle --outfile=./static/notebrew.min.js --watch=forever :: wgo -verbose -file .go go install -tags dev ./notebrew :: notebrew
