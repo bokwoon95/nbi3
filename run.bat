0db7aa87a675c091e1dff74d38d0fb28d9a6de28
bokwoon <bokwoon.c@gmail.com>
2026-04-05 00:49:42 +0800

    checkpoint

--- run.bat
+++ run.bat
@@ -1,17 +1,14 @@
 : #!/bin/sh
 : # This script works in both cmd.exe and sh.
 : # https://stackoverflow.com/a/17623721
-
-:<<'::STOP'
+: <<'::HEREDOC'
 
 @echo off
 set CGO_ENABLED=0
 wgo -xdir . npx @tailwindcss/cli --input ./static/notebrew.css --output ./static/notebrew.min.css --watch=always :: wgo -xdir . npx esbuild ./static/notebrew.js --bundle --outfile=./static/notebrew.min.js --watch=forever :: wgo -verbose -file .go go install -tags dev ./notebrew :: notebrew
-goto :END
+exit /b
 
-::STOP
+::HEREDOC
 
 CGO_ENABLED=0 wgo -xdir . npx @tailwindcss/cli --input ./static/notebrew.css --output ./static/notebrew.min.css --watch=always :: wgo -xdir . npx esbuild ./static/notebrew.js --bundle --outfile=./static/notebrew.min.js --watch=forever :: wgo -verbose -file .go go install -tags dev ./notebrew :: notebrew
 exit $?
-
-:END
